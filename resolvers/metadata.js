8644c48d020a884373bc28a67019f77fe1db5874
anurag sharma <vbanurag.sharma@gmail.com>
2020-11-29 09:43:10 +0800

    setup app

---
+++ resolvers/metadata.js
@@ -0,0 +1,116 @@
+
+require("console-pretty-print");
+
+const AWS = require("aws-sdk");
+
+const isLambda = !!(process.env.LAMBDA_TASK_ROOT || false);
+
+if (isLambda) {
+    AWS.config.update({
+        region: process.env.DROP_AWS_REGION
+    });
+} else {
+    require("dotenv").config();
+    console.log({
+        accessKeyId: process.env.DROP_AWS_ACCESS_ID,
+        secretAccessKey: process.env.DROP_AWS_SECRET_KEY,
+        region: process.env.DROP_AWS_REGION
+    })
+    AWS.config.update({
+        accessKeyId: process.env.DROP_AWS_ACCESS_ID,
+        secretAccessKey: process.env.DROP_AWS_SECRET_KEY,
+        region: process.env.DROP_AWS_REGION
+    });
+}
+
+// INIT AWS
+// env variables set on Lambda function in AWS console
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const defaultParams = {
+    TableName: "Metadata",
+
+    AttributesToGet: [
+        "ogTitle",
+        "ogDescription",
+        "ogKeywords",
+        "ogImages",
+    ]
+};
+
+const fetch = require('isomorphic-fetch');
+const cheerio = require('cheerio')
+
+const _getDataFromUrl = async url => {
+    const response = await fetch(url)
+    const parseText = await response.text();
+    const $ = cheerio.load(parseText);
+    let data = {
+        title: $('h1').text(),
+        description: $('meta[name="description"]').attr('content'),
+        // Get OG Values
+        ogTitle: $('meta[property="og:title"]').attr('content'),
+        ogImages: $('meta[property="og:image"]').attr('content'),
+        ogKeywords: $('meta[property="og:keywords"]').attr('content'),
+        ogDescription: $('meta[property="og:description"]').attr('content'),
+    }
+    if (data.ogTitle && data.ogTitle.trim().length === 0) {
+        data.ogTitle = data.title;
+    }
+    return data;
+}
+
+const checkCache = async params => {
+
+    return new Promise((resolve, reject) => {
+        docClient.get(params, (err, data) => {
+            if (err) {
+                console.log("error getting from dynamodb", err);
+                reject({});
+            } else {
+                console.log("yay got data from dynamodb", data);
+                resolve(data.Item);
+            }
+        });
+    });
+}
+
+const setCache = async params => {
+    return new Promise((resolve, reject) => {
+        docClient.put(params, (err, data) => {
+            if (err) {
+                console.log("error getting from dynamodb", err);
+                reject({});
+            } else {
+                console.log("yay got data from dynamodb", data);
+                resolve(data);
+            }
+        });
+    });
+}
+
+const fetchOgMetaData = async url => {
+    const params = {
+        ...defaultParams,
+        Key: {
+            url: url
+        }
+    };
+    let _dataFound = await checkCache(params);
+    if (_dataFound && Object.keys(_dataFound).length > 0) {
+        return _dataFound;
+    }
+    let data = await _getDataFromUrl(url);
+    data.url = url;
+    setCache({
+        TableName: 'Metadata',
+        Item: data
+    })
+    return data;
+};
+
+
+module.exports = {
+    fetchOgMetaData,
+};
---
+++ schema.js
@@ -0,0 +1,42 @@
+const { gql } = require("apollo-server-lambda");
+const { fetchOgMetaData } = require('./resolvers/metadata.js')
+
+const typeDefs = gql`
+  type Metadata @cacheControl(maxAge: 300) {
+    ogTitle: String
+    ogDescription: String
+    ogImages: [String]
+    ogKeywords: [String]
+  }
+
+  type Query {
+    getOgMetaData(url: String!): Metadata
+  }
+`;
+
+const resolvers = {
+    Query: {
+        getOgMetaData(obj, args, context, info) {
+            return fetchOgMetaData(args.url);
+        },
+    }
+};
+
+const mocks = {
+
+};
+
+module.exports = {
+    typeDefs,
+    resolvers,
+    mocks,
+    mockEntireSchema: false,
+    context: ({ event, context }) => ({
+        headers: event.headers,
+        functionName: context.functionName,
+        event,
+        context
+    }),
+    playground: true,
+    introspection: true,
+};
